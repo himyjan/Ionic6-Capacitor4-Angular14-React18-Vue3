4e04f784cef72b93ceaa3348f7e2e623df35119c
himyjan <51815522+himyjan@users.noreply.github.com>
2022-10-13 00:13:12 +0800

    update react router version

--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -1,14 +1,61 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+// import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from 'react-router-dom';
+
+import Tab1 from './pages/Tab1';
+import Tab2 from './pages/Tab2';
+import Tab3 from './pages/Tab3';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Tab1 />,
+  },
+  {
+    path: '/tab2',
+    element: <Tab2 />,
+  },
+  {
+    path: '/tab3',
+    element: <Tab3 />,
+  },
+  {
+    path: '*',
+    element: <Navigate to='/tab1' replace={true} />,
+  },
+]);
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
